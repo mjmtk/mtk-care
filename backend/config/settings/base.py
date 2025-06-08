abdf24dc9a9e88b58c3ec9cc22c0f1009c7117b2
aftabmj <za@mm.st>
2025-06-08 17:06:27 +1200

    removing CORS handling in Django

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -63,7 +63,7 @@ AUTH_USER_MODEL = 'users.User'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware', # let Azure handle CORS
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
